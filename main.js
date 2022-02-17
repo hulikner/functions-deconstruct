a62b8c4fb90143a7606228c690548a7d7cdbc17e
Hulikner <hulikner@gmail.com>
2022-02-17 13:16:49 -0600

    functions-deconstruct

---
+++ main.js
@@ -0,0 +1,35 @@
+const setOut = () => {
+    console.log('Set steak out 30 mins prior to grilling.')
+}
+
+const preHeat = () => {
+    console.log('Preheat the grill to med-high.')
+}
+
+const season = () => {
+    console.log('Season generously with Himilayan Pink Sea Salt, Black Pepper, and White Pepper.')
+}
+
+const grill = () => {
+    console.log('Grill for 4-5 minutes.')
+}
+
+const flip = () => {
+    console.log('Flip the steak.')
+}
+
+const takeOff = () => {
+    console.log('After 4-5 more minutes on the other side, take off grill.')
+}
+
+const rest = () => {
+    console.log('Let rest for 5 minutes.')
+}
+
+setOut()
+preHeat()
+season()
+grill()
+flip()
+takeOff()
+rest()
